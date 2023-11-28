eb364599c0759bb7eff8d8bca4f2acb744af4e86
Daniel Pinto <daniel2190558@correo.uis.edu.co>
2023-11-27 22:13:58 -0500

    estructura de carpetas

---
+++ middlewares/error.handler.js
@@ -0,0 +1,12 @@
+function errorHandler(err, req, res, next) {
+  if(err.isBoom){
+
+    const { output } = err;
+    res.status(output.statusCode).json({output});
+
+  }else{
+    next(err);
+  }
+}
+
+module.exports = errorHandler;
