6effe1b2f9aff0c1657d302c3ccb0281f8396dfc
Daniel Pinto <daniel2190558@correo.uis.edu.co>
2023-11-30 13:14:55 -0500

    auth-security

---
+++ util/auth/strategies/local.strategy.js
@@ -0,0 +1,28 @@
+/* const { Strategy } = require('passport-local');
+const boom = require('@hapi/boom');
+const {comparePassword} = require('./../../security/password.hash'); */
+
+/* const UserService = require('./../../../services/user.service'); */ // TODO: UserService.
+/* const service = new UserService(); */
+
+/* const LocalStrategy = new Strategy({
+  usernameField: 'email',
+  },
+  async (email, password, done) => {
+  try {
+    const user = await service.findByEmail(email);
+    if(!user){
+      done(boom.unauthorized(), false);
+    }
+    const isMacth = await comparePassword(password, user.password);
+    if(!isMacth){
+      done(boom.unauthorized(), false);
+    }
+    delete user.dataValues.password;
+    done(null, user);
+  } catch (error) {
+    done(error, false);
+  }
+});
+
+module.exports = LocalStrategy; */
