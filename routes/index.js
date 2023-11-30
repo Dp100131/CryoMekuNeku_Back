3060142bf548fdfba5d5435f18bcb122a778fdf3
Daniel Pinto <daniel2190558@correo.uis.edu.co>
2023-11-30 12:50:57 -0500

    remake

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,8 @@
 const express = require("express");
-const userRouter = require("./user.router");
-const userTypeRouter = require("./userType.router");
-const userVideoGamesRouter = require("./userVideoGames.router");
-const videoGamesRouter = require("./videoGame.router");
 
 function routerApi(app) {
   const router = express.Router();
-  app.use('/api/v1', router);
-  router.use('/users', userRouter);
-  router.use('/usersType', userTypeRouter);
-  router.use('/usersVideoGames', userVideoGamesRouter);
-  router.use('/videoGames', videoGamesRouter);
+  app.use('/api/v1', router); 
 }
 
 module.exports = routerApi;
